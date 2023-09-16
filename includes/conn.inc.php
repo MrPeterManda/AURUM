249c3ca616ba5a0344edfd8921cd21241ab1360b
MrPeterManda <peter@wethinkcode.co.za>
2023-09-16 21:28:43 +0200

    db password changed to admin

--- includes/conn.inc.php
+++ includes/conn.inc.php
@@ -1,8 +1,8 @@
 <?php
 
-$serverName = "db"; // This should match the service name defined in your docker-compose.yml
-$dbUsername = "root"; // MySQL root user (as defined in docker-compose.yml)
-$dbPassword = "password"; // MySQL root password (as defined in docker-compose.yml)
+$serverName = "localhost"; // This should match the service name defined in your docker-compose.yml
+$dbUsername = "admin"; // MySQL root user (as defined in docker-compose.yml)
+$dbPassword = "admin"; // MySQL root password (as defined in docker-compose.yml)
 $dbName = "withu"; // Database name
 
 $conn = mysqli_connect($serverName,  $dbUsername, $dbPassword, $dbName);
