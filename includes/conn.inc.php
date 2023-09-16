5ae594e57e43aad3b6b1cdbd8b81792c29bc40a8
MrPeterManda <peter@wethinkcode.co.za>
2023-09-16 11:54:42 +0200

    print message when db connection is ok

--- includes/conn.inc.php
+++ includes/conn.inc.php
@@ -9,4 +9,6 @@ $conn = mysqli_connect($serverName,  $dbUsername, $dbPassword, $dbName);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected to the database successfully!";
 }
