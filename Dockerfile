63b5133406780e3dce5a925bb6db0beaf550f175
MrPeterManda <peter@wethinkcode.co.za>
2023-09-16 11:34:52 +0200

    connecting database

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,14 @@ FROM php:8.2-apache
 # Set the working directory in the container
 WORKDIR /var/www/html
 
+# Install any PHP extensions or dependencies your app requires
+RUN docker-php-ext-install mysqli pdo pdo_mysql
+
 # Copy your PHP application files to the container
 COPY . .
 
 # Copy Firstcampaign.php to index.php (our new entry point)
 RUN cp Firstcampaign.php index.php
-
-# Install any PHP extensions or dependencies your app requires
-RUN docker-php-ext-install mysqli
 
 # Expose port 80 for Apache
 EXPOSE 80
--- includes/conn.inc.php
+++ includes/conn.inc.php
@@ -1,13 +1,12 @@
 <?php
 
-$serverName = "localhost";
-$dbUsername = "admin";
-$dbPassword = "admin";
-$dbName = "withu";
+$serverName = "db"; // This should match the service name defined in your docker-compose.yml
+$dbUsername = "root"; // MySQL root user (as defined in docker-compose.yml)
+$dbPassword = "password"; // MySQL root password (as defined in docker-compose.yml)
+$dbName = "withu"; // Database name
 
 $conn = mysqli_connect($serverName,  $dbUsername, $dbPassword, $dbName);
 
-if(!$conn){
-    die ("Connection failed".mysqli_connect_error());
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
-
